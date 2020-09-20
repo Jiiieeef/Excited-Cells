c2e68f86da1f9eee8517a5946e1cd7d0240dde4f
Jean-Franck Ballet <ballet.jean.franck@gmail.com>
2020-09-20 14:40:51 +0200

    Add algorithm with tests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,70 @@
-function excitedCells() {
-  return true;
+const QUIET = 0;
+const EXCITED = 1;
+
+function getNeighbors(index, cells) {
+  let previous, nextCell;
+
+  if (index === 0) {
+    previous = cells[cells.length - 1];
+    nextCell = cells[index + 1];
+  } else if (index === cells.length - 1) {
+    previous = cells[index - 1];
+    nextCell = cells[0];
+  } else {
+    previous = cells[index - 1];
+    nextCell = cells[index + 1];
+  }
+
+  return [previous, nextCell];
 }
 
-module.exports = excitedCells;
+const getNumbersOfExcitedCells = neighbors =>
+  neighbors.filter(neighbor => neighbor === EXCITED).length
+
+function emitSignals(cells) {
+  return cells.map((cell, index) => {
+    const neighbors = getNeighbors(index, cells);
+    let newValue = cell;
+
+    if (cell === QUIET) {
+      if (getNumbersOfExcitedCells(neighbors) === 1) {
+        newValue = EXCITED;
+      }
+    } else {
+      if (getNumbersOfExcitedCells(neighbors) !== 1) {
+        newValue = QUIET;
+      }
+    }
+
+    return newValue;
+  });
+}
+
+function excitedCells(cells, step) {
+  if (typeof step !== 'number') {
+    throw new Error('step must be a number.');
+  }
+  if (step < 1) {
+    throw new Error('step must be superior to 0.');
+  }
+
+  let currentStep = 1;
+  let result = cells;
+
+  while(currentStep <= step) {
+    result = emitSignals(result);
+
+    console.log(`Step #${currentStep}: ${result.join(',')}`);
+
+    currentStep++;
+  }
+
+  return result.join(',');
+}
+
+module.exports = {
+  excitedCells,
+  getNeighbors,
+  getNumbersOfExcitedCells,
+  emitSignals
+};
--- test/index.test.js
+++ test/index.test.js
@@ -1,8 +1,59 @@
 const expect = require('chai').expect;
-const excitedCells = require('../src/index');
+const { getNeighbors, getNumbersOfExcitedCells, emitSignals, excitedCells } = require('../src/index');
 
 describe('Excited Cells', function () {
-  it('should return true', function () {
-    expect(excitedCells()).to.be.true;
+  describe('getNeighbors', function() {
+    const array = [1, 2, 3, 4, 5];
+
+    it('should return [5, 2] when idex is 0', function() {
+      expect(getNeighbors(0, array)).to.eql([5, 2]);
+    });
+
+    it('should return [2, 4] when idex is 2', function() {
+      expect(getNeighbors(2, array)).to.eql([2, 4]);
+    });
+
+    it('should return [4, 1] when idex is 4', function() {
+      expect(getNeighbors(4, array)).to.eql([4, 1]);
+    });
+  });
+
+  describe('getNumbersOfExcitedCells', function() {
+    it('should return 0 when there is no excited cells', function() {
+      expect(getNumbersOfExcitedCells([0, 0])).to.equal(0);
+    });
+
+    it('should return 1 when the array is [0, 1]', function() {
+      expect(getNumbersOfExcitedCells([0, 1])).to.equal(1);
+    });
+
+    it('should return 2 when the array is [1, 1]', function() {
+      expect(getNumbersOfExcitedCells([1, 1])).to.equal(2);
+    });
+  });
+
+  describe('emitSignals', function() {
+    it('should return [1, 0, 1, 0] when the array is [1, 0, 1, 1]', function() {
+      expect(emitSignals([1, 0, 1, 1])).to.eql([1, 0, 1, 0]);
+    });
+
+    it('should return [0, 0, 0, 0] when the array is [1, 0, 1, 0]', function() {
+      expect(emitSignals([1, 0, 1, 0])).to.eql([0, 0, 0, 0]);
+    });
+  });
+
+  describe('excitedCells', function() {
+    it('should return a string with the new state of the cells', function () {
+      expect(excitedCells([1, 0, 1, 1], 1)).to.equal('1,0,1,0');
+      expect(excitedCells([1, 0, 1, 1], 2)).to.equal('0,0,0,0');
+    });
+
+    it('should return an error when the step is inferior to 1', function() {
+      expect(() => excitedCells([1, 1, 1], 0)).to.throw('step must be superior to 0.');
+    });
+
+    it('should return an error when the step is not a number', function() {
+      expect(() => excitedCells([1, 1, 1], 'hey')).to.throw('step must be a number.');
+    });
   });
 });
